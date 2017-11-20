a4c6a62e2c4f0f05dbf62a3f9d36839734d2f98e
eltimsy <timothy.sc.tse@gmail.com>
2017-11-20 09:40:23 -0500

    starting to play with scraping a little

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const url = 'https://www.healthgrades.com/physician/dr-emefre-udo-yp98t';
+
+  request(url, function(error, response, html) {
+    if(!error) {
+      let $ = cheerio.load(html);
+
+      let doctor, total, rating;
+      let json = {doctor: "", total: "", rating: ""};
+    }
+  });
   const data = {
     stuff: 'abc'
   };
