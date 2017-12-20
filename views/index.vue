a7210f4b84ab7563ddadaa0b6af6577ae3ba0704
eltimsy <timothy.sc.tse@gmail.com>
2017-12-20 09:07:44 -0500

    adding radio buttons to frontend page

--- views/index.vue
+++ views/index.vue
@@ -4,6 +4,18 @@
     <form method=get v-on:submit.prevent="getData()">
       <label>Please enter Doctor Name and Specialty</label>
       <input class="form-control" v-model="doctor" placeholder="Name + Specialty"><br>
+      <label class="form-check-label">
+        <input class="form-check-input" type="checkbox" v-model="healthgrades">
+        Healthgrades
+      </label>
+      <label class="form-check-label">
+        <input class="form-check-input" type="checkbox" v-model="vitals">
+        Vitals
+      </label>
+      <label class="form-check-label">
+        <input class="form-check-input" type="checkbox" v-model="ratemds">
+        Ratemds
+      </label>
       <input class="btn btn-warning" type=submit value=Submit>
     </form>
     <br>
@@ -38,13 +50,19 @@ export default {
     return {
       doctor: '',
       values: '',
+      healthgrades: false,
+      vitals: false,
+      ratemds: false,
     }
   },
   methods: {
     getData: function() {
       axios.get('/doctorapi', {
         params: {
-          doctor: this.doctor
+          doctor: this.doctor,
+          healthgrades: this.healthgrades,
+          vitals: this.vitals,
+          ratemds: this.ratemds,
         }
       }).then(response => {
         this.values = response.data;
