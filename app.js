95d3589714e61c69e92ce39e8181dadae4f75b07
eltimsy <timothy.sc.tse@gmail.com>
2018-01-19 12:41:35 -0500

    adding strict to main file

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
+'use strict';
 require('dotenv').config()
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var expressVue = require('express-vue');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressVue = require('express-vue');
+const ipfilter = require('express-ipfilter').IpFilter;
+const IpDeniedError = require('express-ipfilter').IpDeniedError;
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://localhost:5432/ttse', { operatorsAliases: false });
+const sequelize = new Sequelize(process.env.DB_HOST, { operatorsAliases: false });
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var doctorAPI = require('./routes/doctorAPI');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const doctorAPI = require('./routes/doctorAPI');
 const googleMapsClient = require('@google/maps').createClient({
   key: process.env.GOOGLE_MAPS_API_KEY
 });
 
-var app = express();
+var ip = require('ip');
+console.log(ip.address())
+const app = express();
 
 // view engine setup
 const vueOptions = {
@@ -28,7 +33,6 @@ const vueOptions = {
 };
 const expressVueMiddleware = expressVue.init(vueOptions);
 app.use(expressVueMiddleware);
-
 app.set('sequelize', sequelize)
 app.set('googleMapsClient', googleMapsClient)
 // uncomment after placing your favicon in /public
@@ -39,16 +43,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const ips = ['::ffff:127.0.0.1', '::1', '74.213.184.33'];
+app.use(ipfilter(ips, {mode: 'allow'}));
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/doctorapi', doctorAPI);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
+
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, _next) {
+    console.log('Error handler', err);
+    if(err instanceof IpDeniedError){
+      res.status(401);
+    }else{
+      res.status(err.status || 500);
+    }
+
+    res.renderVue('error', {
+      message: 'You shall not pass',
+      error: err
+    });
+  });
+}
 
 // error handler
 app.use(function(err, req, res, next) {
